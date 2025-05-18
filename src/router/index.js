9ab7cfea3662de411249352fc6727eac686ab89a
kagari7 <133572575+kagari7@users.noreply.github.com>
2025-05-18 17:43:43 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,9 @@ import SongManage from "@/views/SongManage.vue";
 import SongDetail from "@/views/SongDetail.vue";
 import AlbumDetail from "@/views/AlbumDetail.vue";
 import ArtistDetail from "@/views/ArtistDetail.vue";
+import UserLikes from "@/views/UserLikes.vue";
 import UserManage from "@/views/UserManage.vue";
-import ProfileCenter from "@/views/ProfileCenter.vue";
-import Account from "@/views/Account.vue";
-
+import PlaylistManage from "@/views/PlaylistManage.vue";
 
 const routes = [
     { path: '/', component: Login },
@@ -21,12 +20,12 @@ const routes = [
     { path: '/music_manage', component: ArtistManage },
     { path: '/album_manage', component: AlbumManage },
     { path: '/song_manage', component: SongManage },
-    { path: '/user_manage', component: UserManage},
     { path: "/song_detail/:id", component: SongDetail},
     { path: '/album_detail/:id', component: AlbumDetail },
     { path: '/artist_detail/:id', component: ArtistDetail },
-    { path: '/profile_center',component: ProfileCenter},
-    { path: '/account', component: Account },
+    { path: '/user_likes', component: UserLikes },
+    { path: '/user_manage', component: UserManage },
+    { path: '/playlist_manage', component: PlaylistManage },
 ]
 
 const router = createRouter({
@@ -35,3 +34,4 @@ const router = createRouter({
 })
 
 export default router;
+
